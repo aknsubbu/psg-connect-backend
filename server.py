e659f81fb1c569fc3cd0d540bb9b8be4bb557e50
aknsubbu <aknsubbu@gmail.com>
2024-09-30 19:31:55 +0530

    trying again

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import uvicorn
 from dataFetchFunctions import CAMarksWebScrapper
 from dataExceptions import InvalidUsernameOrPasswordException, ScrappingError, NoSemResultsAvailable, NoCAMarksAvailable, AttendanceUpdateInProcessException, NoTimeTableDataException
 
@@ -55,3 +56,8 @@ async def fetch_data(credentials: Credentials):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
