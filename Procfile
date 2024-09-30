f89e6bbc66f27769fca1e736f619ae11c217887a
aknsubbu <aknsubbu@gmail.com>
2024-09-30 19:39:58 +0530

    i just keep trying but nothing works

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn -w 2 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --worker-tmp-dir /dev/shm server:app
--- server.py
+++ server.py
@@ -59,5 +59,5 @@ async def fetch_data(credentials: Credentials):
 
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
